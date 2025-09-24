f9df95c24be3952b0a993354b24e39f76bf52e16
Arnoud <varnoud67@gmail.com>
2025-09-24 11:54:42 +0200

    Making the CompareComps
    add CompareComps class with byte comparison methods

--- domein/CompareComps.java
+++ domein/CompareComps.java
@@ -1,7 +1,5 @@
 package domein;
 
-import java.util.Iterator;
-
 public class CompareComps {
 	private LogicGates lg;
 	
@@ -10,113 +8,63 @@ public class CompareComps {
 	}
 	
 	public boolean equal(boolean[] byteOne, boolean[] byteTwo) {
-		return lg.notGate(lg.xorGate(byteOne[0], byteOne[0]));
+	    boolean e0 = lg.notGate(lg.xorGate(byteOne[0], byteTwo[0]));
+	    boolean e1 = lg.notGate(lg.xorGate(byteOne[1], byteTwo[1]));
+	    boolean e2 = lg.notGate(lg.xorGate(byteOne[2], byteTwo[2]));
+	    boolean e3 = lg.notGate(lg.xorGate(byteOne[3], byteTwo[3]));
+	    boolean e4 = lg.notGate(lg.xorGate(byteOne[4], byteTwo[4]));
+	    boolean e5 = lg.notGate(lg.xorGate(byteOne[5], byteTwo[5]));
+	    boolean e6 = lg.notGate(lg.xorGate(byteOne[6], byteTwo[6]));
+	    boolean e7 = lg.notGate(lg.xorGate(byteOne[7], byteTwo[7]));
+
+	    return lg.andGate(
+	             lg.andGate(lg.andGate(e0, e1), lg.andGate(e2, e3)),
+	             lg.andGate(lg.andGate(e4, e5), lg.andGate(e6, e7))
+	           );
 	}
 	
 	public boolean notEqual(boolean[] byteOne, boolean[] byteTwo) {
 		return lg.notGate(equal(byteOne, byteTwo));
 	}
 	
-	public boolean biggerThen(boolean[] byteOne, boolean[] byteTwo) {
-		return true;
+	public boolean greaterThen(boolean[] byteOne, boolean[] byteTwo) {
+	    boolean gt7 = lg.andGate(lg.notGate(byteTwo[0]), byteOne[0]);
+	    boolean eq7 = lg.notGate(lg.xorGate(byteOne[0], byteTwo[0]));
+
+	    boolean gt6 = lg.andGate(lg.andGate(lg.notGate(byteTwo[1]), byteOne[1]), eq7);
+	    boolean eq6 = lg.andGate(lg.notGate(lg.xorGate(byteOne[1], byteTwo[1])), eq7);
+
+	    boolean gt5 = lg.andGate(lg.andGate(lg.notGate(byteTwo[2]), byteOne[2]), eq6);
+	    boolean eq5 = lg.andGate(lg.notGate(lg.xorGate(byteOne[2], byteTwo[2])), eq6);
+
+	    boolean gt4 = lg.andGate(lg.andGate(lg.notGate(byteTwo[3]), byteOne[3]), eq5);
+	    boolean eq4 = lg.andGate(lg.notGate(lg.xorGate(byteOne[3], byteTwo[3])), eq5);
+
+	    boolean gt3 = lg.andGate(lg.andGate(lg.notGate(byteTwo[4]), byteOne[4]), eq4);
+	    boolean eq3 = lg.andGate(lg.notGate(lg.xorGate(byteOne[4], byteTwo[4])), eq4);
+
+	    boolean gt2 = lg.andGate(lg.andGate(lg.notGate(byteTwo[5]), byteOne[5]), eq3);
+	    boolean eq2 = lg.andGate(lg.notGate(lg.xorGate(byteOne[5], byteTwo[5])), eq3);
+
+	    boolean gt1 = lg.andGate(lg.andGate(lg.notGate(byteTwo[6]), byteOne[6]), eq2);
+	    boolean eq1 = lg.andGate(lg.notGate(lg.xorGate(byteOne[6], byteTwo[6])), eq2);
+
+	    boolean gt0 = lg.andGate(lg.andGate(lg.notGate(byteTwo[7]), byteOne[7]), eq1);
+
+	    return lg.orGate(
+	             lg.orGate(lg.orGate(gt7, gt6), lg.orGate(gt5, gt4)),
+	             lg.orGate(lg.orGate(gt3, gt2), lg.orGate(gt1, gt0)));
 	}
 	
-    //Temp methodes
-    public boolean[] numToByte(int num) {
-        boolean[] outputByte = new boolean[9];
-        outputByte[0] = num > 255;
-        boolean b7 = num >= 128; num = b7 ? num - 128 : num;
-        boolean b6 = num >= 64;  num = b6 ? num - 64 : num;
-        boolean b5 = num >= 32;  num = b5 ? num - 32 : num;
-        boolean b4 = num >= 16;  num = b4 ? num - 16 : num;
-        boolean b3 = num >= 8;   num = b3 ? num - 8 : num;
-        boolean b2 = num >= 4;   num = b2 ? num - 4 : num;
-        boolean b1 = num >= 2;   num = b1 ? num - 2 : num;
-        boolean b0 = num >= 1;   num = b0 ? num - 1 : num;
-
-        outputByte[1] = b7;
-        outputByte[2] = b6;
-        outputByte[3] = b5;
-        outputByte[4] = b4;
-        outputByte[5] = b3;
-        outputByte[6] = b2;
-        outputByte[7] = b1;
-        outputByte[8] = b0;
-
-        return outputByte;
-    }
-
-    public int byteToNum(boolean[] inputByte) {
-        int num = 0;
-        if (inputByte[1]) num += 128;
-        if (inputByte[2]) num += 64;
-        if (inputByte[3]) num += 32;
-        if (inputByte[4]) num += 16;
-        if (inputByte[5]) num += 8;
-        if (inputByte[6]) num += 4;
-        if (inputByte[7]) num += 2;
-        if (inputByte[8]) num += 1;
-        if (inputByte[0]) System.out.println("overflow");
-        return num;
-    }
-    
-    public int byteToNumNeg(boolean[] inputByte) {
-        int num = 0;
-        if (inputByte[1]) num -= 128;
-        if (inputByte[2]) num += 64;
-        if (inputByte[3]) num += 32;
-        if (inputByte[4]) num += 16;
-        if (inputByte[5]) num += 8;
-        if (inputByte[6]) num += 4;
-        if (inputByte[7]) num += 2;
-        if (inputByte[8]) num += 1;
-        if (inputByte[0]) System.out.println("overflow");
-        return num;
-    }
-
-    public static void main(String[] args) {
-        CompareComps cc = new CompareComps();
-        boolean[] results = new boolean[256 * 256];
-
-        int index = 0;
-
-        for (int b = 0; b < 256; b++) {
-            for (int a = 0; a < 256; a++) {
-                results[index] = (a == b);
-                index++;
-            }
-        }
-
-        boolean test = true;
-        for (int b = 0; b < 256; b++) {
-            for (int a = 0; a < 256; a++) {
-                if (!cc.equal(cc.numToByte(a), cc.numToByte(b))) {
-                    test = false;
-                    break;
-                }
-            }
-            if (!test) break;
-        }
-
-        if (test) {
-            System.out.println("The test succeeded");
-        } else {
-            System.out.println("The test failed");
-        }
-    }
+	public boolean greaterOrEqualThen(boolean[] byteOne, boolean[] byteTwo) {
+		return lg.orGate(equal(byteOne, byteTwo), greaterThen(byteOne, byteTwo));
+	}
+	
+	public boolean lesserThen(boolean[] byteOne, boolean[] byteTwo) {
+		return lg.andGate(lesserOrEqualThen(byteOne, byteTwo), notEqual(byteOne, byteTwo));
+	}
+	
+	public boolean lesserOrEqualThen(boolean[] byteOne, boolean[] byteTwo) {
+		return lg.notGate(greaterThen(byteOne, byteTwo));
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
